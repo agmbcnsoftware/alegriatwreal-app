e58abdb0c580f115d2d982b8901f7e62018bd920
Glitch (flask-demo) <none>
2024-11-24 08:31:10 +0000

    🌸🚲 Checkpoint ./server.py:72021626/238

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 from flask import request
 import model
@@ -8,23 +8,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  print(f"Received data")
-  data = request.get_json()
-  print("Received data:", data)
+  print(f"Received data in route /")
   return "Hello world!"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
+    print("Received webhook")
     try:
         data = request.get_json()  # Extrae JSON
-        print("Received data:", data)
+        print("With data:", data)
         if not data:
             return jsonify({"error": "No JSON received"}), 400
-            print("no data received")
-        #return jsonify({"message": "Webhook received successfully!"}), 200
+        return jsonify({"message": "Webhook received successfully!"}), 200
     except Exception as e:
         print("Error:", e)
-        #return jsonify({"error": "Invalid JSON"}), 400
+        return jsonify({"error": "Invalid JSON"}), 400
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=3000)
