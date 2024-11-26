4eb36118cc3f3b4f31bce1ba677ba87c7e2681c6
Glitch (flask-demo) <none>
2024-11-26 18:55:48 +0000

    💰😚 Checkpoint ./server.py:72021626/151

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, request
 from twilio.rest import Client
+import openai
 import os
+
+# Configura OpenAI API Key
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 app = Flask(__name__)
 
@@ -8,6 +13,7 @@ app = Flask(__name__)
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 twilio_number = 'whatsapp:' + os.getenv("TWILIO_NUMBER")  # Ejemplo: 'whatsapp:+14155238886'
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 client = Client(account_sid, auth_token)
 
