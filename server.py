414fea7518615f67ead268b9f267046692b88276
Glitch (flask-twilio-integration-poc) <none>
2024-12-29 17:58:50 +0000

    😇🐟 Checkpoint ./server.py:72021626/34

--- server.py
+++ server.py
@@ -102,7 +102,7 @@ def get_appointments_from_mail():
             # Inserto la información que me llega en los emails en base de datos
             user_id = db. get_or_create_user(whatsapp_number, nombre)
             db.get_or_create_reservation(user_id, whatsapp_number, clase, horario, class_date, class_time)
-            db.print_all_reservations()
+        #db.print_all_reservations()
 
 def notify_appointments():   
     print("Enviando notificaciones)") 
@@ -126,7 +126,7 @@ def notify_appointments():
         
 def start_appointment_notifications():
     #schedule.every().minute.at(":00").do(notify_appointments)
-    schedule.every().minute.at(":00").do(get_appointments_from_mail)
+    #schedule.every().minute.at(":00").do(get_appointments_from_mail)
     schedule.every().day.at("08:00").do(notify_appointments)
     while True:
         schedule.run_pending()
