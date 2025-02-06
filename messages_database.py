d7aa9059434a1c08a4c2042e68caba8ebcb2f3a3
Glitch (alegriatwreal) <none>
2025-02-06 09:29:37 +0000

    🦀⏳ Checkpoint ./messages_database.py:72021626/12 ./templates/reservations.html:72021626/135

--- messages_database.py
+++ messages_database.py
@@ -277,7 +277,7 @@ def get_all_reservations():
         cursor = conn.cursor()
         # Obtener reservas del día actual sin recordatorio enviado
         cursor.execute("""
-        SELECT id, user_name, user_surname, whatsapp_number, class_type, class_weekday_hour, class_date, class_time, reminder_sent
+        SELECT id, user_name, user_surname, whatsapp_number, class_type, class_weekday_hour, class_date, class_time, reminder_sent, created_at
         FROM trial_class_reservations;
         """)
         
