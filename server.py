aca77ef2ccd8a73457e329362647b91fce509e54
Glitch (alegriatwreal) <none>
2025-02-17 18:21:31 +0000

    😄🖖 Checkpoint ./ai-info-base.txt:72021626/14640 ./server.py:72021626/1

--- server.py
+++ server.py
@@ -153,7 +153,7 @@ def notify_appointments():
     print("Notificaciones enviadas")   
                 
 def start_appointment_notifications():
-    get_appointments_from_mail()
+    #get_appointments_from_mail()
     schedule.every(15).minutes.do(get_appointments_from_mail)
     schedule.every().day.at("08:03").do(notify_appointments)
     while True:
