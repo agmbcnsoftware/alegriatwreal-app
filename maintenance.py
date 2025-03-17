4dbdf9a938a722e5f341e4622d63d004f9cd3634
Glitch (alegriatwreal) <none>
2025-03-17 18:25:25 +0000

    🍗🎂 Checkpoint ./maintenance.py:72021626/1528 ./start.sh:72021626/4

--- maintenance.py
+++ maintenance.py
@@ -23,19 +23,20 @@ def delete_reservations_from_user_date(from_number, class_date):
         conn.commit()
         print(f"Mensajes eliminados para el número: {from_number} y fecha: {class_date}")
         
-def update_reservation_date(reservation_id):
-    with get_connection() as conn:
+def update_reservation_date(reservation_id, reservation_date):
+    with db.get_connection() as conn:
         cursor = conn.cursor()     
         cursor.execute("""
         UPDATE trial_class_reservations
-        SET reminder_sent = 0, class_date = updated_at = CURRENT_TIMESTAMP
+        SET reminder_sent = 0, class_date = ?, updated_at = CURRENT_TIMESTAMP
         WHERE id = ?;
-        """, (reservation_id,))
+        """, (reservation_date, reservation_id,))
         conn.commit()
 
 
 #db.print_all_reservations()    
 #delete_reservations_from_user("whatsapp:+34625740413")
-delete_reservations_from_user_date("whatsapp:+34667285233", "2025-01-27")
+#delete_reservations_from_user_date("whatsapp:+34667285233", "2025-01-27")
+update_reservation_date(105, '2025-03-26')
 #db.print_all_reservations() 
 #send_whatsapps.send_reminder_by_whatsapp("whatsapp:+34658595387", "Alvaro", "SEVILLANAS", "2024-01-22", "14:55")
