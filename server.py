ecf02a8ec6f066cd20b9c5524460f53699b2d54e
Glitch (alegriatwreal) <none>
2025-04-02 13:21:43 +0000

    😚🚶 Checkpoint ./server.py:72021626/2

--- server.py
+++ server.py
@@ -128,7 +128,7 @@ def get_appointments_from_mail():
                 # Inserto la información que me llega en los emails en base de datos
                 user_id = db. get_or_create_user(whatsapp_number, nombre)
                 db.get_or_create_reservation(user_id, nombre, apellidos, whatsapp_number, clase, horario, class_date, class_time)
-                db.insert_new_prospect(user_id, nombre, apellidos, whatsapp_number, correo, clase, class_date)
+                #db.insert_new_prospect(user_id, nombre, apellidos, whatsapp_number, correo, clase, class_date)
             except Exception as e:
             # Captura la excepción y muestra la traza
                 print("No se pudo gestionar el mail de:" + nombre)
@@ -155,7 +155,7 @@ def notify_appointments():
                 
 def start_appointment_notifications():
     #get_appointments_from_mail()
-    schedule.every(1).minutes.do(get_appointments_from_mail)
+    schedule.every(15).minutes.do(get_appointments_from_mail)
     schedule.every().day.at("08:01").do(notify_appointments)
     while True:
         schedule.run_pending()
