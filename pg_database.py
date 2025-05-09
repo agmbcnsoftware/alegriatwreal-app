059c23731d178246c1846b60554b088439e9734e
Glitch (alegriatwreal) <none>
2025-05-09 18:28:47 +0000

    🏐🌻 Checkpoint ./pg_database.py:72021626/5889

--- pg_database.py
+++ pg_database.py
@@ -20,20 +20,22 @@ def get_db_connection():
     
     # Establecer el esquema específico
    
-def get_filtered_messages(filter_option):
+def get_filtered_messages2(filter_option):
     
-    query = "SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM analytics.messages"
+    query = "SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM 'Alegria'.messages"
     params = []
     print("n8nMensajes")
     # Obtener fechas basadas en la opción de filtro
     now = datetime.now()
     
     if filter_option == "today":
+        print("Today")
         start_date = now.replace(hour=0, minute=0, second=0, microsecond=0)
         end_date = now
         query += " WHERE timestamp BETWEEN %s AND %s"
         params = [start_date, end_date]
     elif filter_option == "yesterday":
+        print("Yesterday")
         start_date = (now - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
         end_date = start_date + timedelta(days=1)
         query += " WHERE timestamp BETWEEN %s AND %s"
@@ -58,8 +60,33 @@ def get_filtered_messages(filter_option):
     # Ejecutar la consulta
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute(query, params)
+    query = "SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM 'Alegria'.messages"
+    #cursor.execute(query, params)
+    cursor.execute(query)
     results = cursor.fetchall()
     cursor.close()
     conn.close()
     return results
+  
+  
+  #--------------------------
+  def get_filtered_messages(filter_option):
+    
+    query = "SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM 'Alegria'.messages"
+    params = []
+    print("n8nMensajes")
+    # Obtener fechas basadas en la opción de filtro
+    print("Query:")
+    print(query)
+    # Ejecutar la consulta
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    query = "SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM 'Alegria'.messages"
+    #cursor.execute(query, params)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results
+  
+  
