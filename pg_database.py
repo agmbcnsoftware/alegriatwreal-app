0b373780f45218a2e9b9c615e85f25842e9b8a58
Glitch (alegriatwreal) <none>
2025-05-13 18:04:02 +0000

    🎬🏣 Checkpoint ./pg_database.py:72021626/1982 ./templates/n8nmessages.html:72021626/21 ./server.py:72021626/2

--- pg_database.py
+++ pg_database.py
@@ -32,87 +32,35 @@ def get_db_connection():
         
     # Establecer el esquema específico
     
-    #postgresql://postgres:kNcuqlRsCPWmtqiMzDtmxhhyTYomOjTt@crossover.proxy.rlwy.net:55419/railway
-    
-   
-def get_filtered_messages2(filter_option):
-    
-    query = 'SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM "Alegria".messages'
-    params = []
-    print("n8nMensajes")
-    # Obtener fechas basadas en la opción de filtro
-    now = datetime.now()
-    
-    if filter_option == "today":
-        print("Today")
-        start_date = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        end_date = now
-        query += " WHERE timestamp BETWEEN %s AND %s"
-        params = [start_date, end_date]
-    elif filter_option == "yesterday":
-        print("Yesterday")
-        start_date = (now - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
-        end_date = start_date + timedelta(days=1)
-        query += " WHERE timestamp BETWEEN %s AND %s"
-        params = [start_date, end_date]
-    elif filter_option == "last7days":
-        start_date = now - timedelta(days=7)
-        end_date = now
-        query += " WHERE timestamp BETWEEN %s AND %s"
-        params = [start_date, end_date]
-    elif filter_option == "lastmonth":
-        start_date = now - timedelta(days=30)
-        end_date = now
-        query += " WHERE timestamp BETWEEN %s AND %s"
-        params = [start_date, end_date]
-    elif filter_option == "all":
-        # Sin condiciones adicionales, selecciona todos los mensajes
-        pass
-    else:
-        raise ValueError(f"Opción de filtro no válida: {filter_option}")
-    print("Query:")
-    print(query)
-    # Ejecutar la consulta
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    query = 'SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM "Alegria".messages'
-    #cursor.execute(query, params)
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return results
-  
-  
-  #--------------------------
+
 def get_filtered_messages(filter_option):
     
     #query = 'SELECT whatsapp_number, whatsapp_profile, message, timestamp, sender FROM "Alegria".messages'
     query = 'SELECT u.whatsapp_number, u.whatsapp_profile, m.message, m.timestamp, m.sender FROM "Alegria".messages m JOIN "Alegria".users u ON m.user_id = u.id'
-    
+    params = []
     now = datetime.now()
     
     if filter_option == "today":
         print("Today")
         start_date = now.replace(hour=0, minute=0, second=0, microsecond=0)
         end_date = now
-        query += " WHERE timestamp BETWEEN %s AND %s"
+        query += " WHERE m.timestamp BETWEEN %s AND %s"
         params = [start_date, end_date]
     elif filter_option == "yesterday":
         print("Yesterday")
         start_date = (now - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
         end_date = start_date + timedelta(days=1)
-        query += " WHERE timestamp BETWEEN %s AND %s"
+        query += " WHERE m.timestamp BETWEEN %s AND %s"
         params = [start_date, end_date]
     elif filter_option == "last7days":
         start_date = now - timedelta(days=7)
         end_date = now
-        query += " WHERE timestamp BETWEEN %s AND %s"
+        query += " WHERE m.timestamp BETWEEN %s AND %s"
         params = [start_date, end_date]
     elif filter_option == "lastmonth":
         start_date = now - timedelta(days=30)
         end_date = now
-        query += " WHERE timestamp BETWEEN %s AND %s"
+        query += " WHERE m.timestamp BETWEEN %s AND %s"
         params = [start_date, end_date]
     elif filter_option == "all":
         # Sin condiciones adicionales, selecciona todos los mensajes
@@ -129,7 +77,7 @@ def get_filtered_messages(filter_option):
         
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute(query)
+        cursor.execute(query,params)
         results = cursor.fetchall()
         return results
     
